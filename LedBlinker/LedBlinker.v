ca63245dfa46befdf94ca42192431de4b9e56b93
danglevm <danglevuminh@gmail.com>
2023-09-26 19:48:34 -0400

    LedBlinker first commit

---
+++ LedBlinker/LedBlinker.v
@@ -0,0 +1,19 @@
+module LedBlinker (
+	i_clk
+	i_en
+	i_switch_1
+	i_switch_2
+	o_led
+);
+
+parameter
+
+//1 HZ enable generator
+always @(posedge clk)
+begin
+end
+
+
+//Lit up the LED when enabled
+
+endmodule
